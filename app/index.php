a22a3d171abf51c576dd018e04ccd39f80c48a58
Hernán Adalid <escalera_perez@ucol.mx>
2023-03-02 13:34:34 -0600

    base conexión

--- app/index.php
+++ app/index.php
@@ -1,1 +1,14 @@
-HOLA MUNDO HAEP
+<?php
+$servername = "mysql-service:3306";
+$database = "siceuc";
+$username = "root";
+$password = "super";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+mysqli_close($conn);
+?>
